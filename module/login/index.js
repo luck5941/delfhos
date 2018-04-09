1c7eb59d2b1d10233ad073774181d6e7d9f6a1c3
Lucas Elvira <lucaselvira96@gmail.com>
2018-04-09 23:48:20 +0200

    fix the bug of the login when the user not exist

--- module/login/index.js
+++ module/login/index.js
@@ -10,12 +10,12 @@ function FORM() {
 		let response = ddbb.query({user: data[0]});
 		let ip = socket.handshake.address.split(":").slice(-1)[0];
 		response.then((res) => {
-			res = res[0];
 			if (res.length < 1){ 
 				session[ip]["register"] = false;
 				modules.communication.send({access: false}, data[1], data[2], socket);
 			}
 			else{
+				res = res[0];
 				session[ip]["register"] = true;
 				session[ip]["user"] = res.user
 				modules.communication.send({access: true}, data[1], data[2], socket);
